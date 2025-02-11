cbaa52f37c2025bad623966dde1858c29336377b
tory <voevaul@gmail.com>
2025-02-10 19:18:27 -0500

    first commit

---
+++ src/main/java/net/missing/playingcards/PlayingCardsClient.java
@@ -0,0 +1,10 @@
+package net.missing.playingcards;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlayingCardsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/missing/playingcards/PlayingCardsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.missing.playingcards;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PlayingCardsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
